302c8883d9a542921f130199697038aba1c011ec
Candra-Dv <Candradermawansyah.com>
2023-06-21 10:07:23 +0700

    update responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
+// ganti navbar active ketika diklik
+const navLinks = document.querySelectorAll('#navbar-nav a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    const activeLink = document.querySelector('#navbar-nav a.active');
+    if (activeLink) {
+      activeLink.classList.remove('active');
+    }
+
+    link.classList.add('active');
+  });
+});
+
+// ganti navbar jadi sticky ketika discoll
 const navbar = document.querySelector("nav");
 window.addEventListener ("scroll", function() {
     navbar.classList.toggle("sticky", window.pageYOffset > 100);
