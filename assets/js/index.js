48d57c40c2024fdf31ec385148e39fdd7680f9f1
Candra-Dv <candradermawansyah@gmail.com>
2025-02-15 19:53:00 +0700

    update profil

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,23 @@ const navbar = document.querySelector("nav");
 window.addEventListener ("scroll", function() {
     navbar.classList.toggle("sticky", window.pageYOffset > 100);
 });
+
+// mengirim email
+function sendEmail(event) {
+  event.preventDefault(); // Mencegah reload halaman
+
+  // Ambil data dari input
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+
+  // Encode agar tidak terjadi error pada URL
+  subject = encodeURIComponent(subject);
+  message = encodeURIComponent(message);
+
+  // Buat link mailto
+  var mailtoLink = `https://mail.google.com/mail/?view=cm&to=candradermawansyah@gmail.com&su=${subject}&body=${message}`;
+
+  // Arahkan pengguna ke Gmail dengan email yang sudah terisi
+  window.open(mailtoLink, "_blank");
+}
